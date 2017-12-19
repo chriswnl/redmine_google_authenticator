b00dbc23cd0b48e68dbcb07dce37bbf7fad5b3d4
Chris Willis <chris@twigtelecom.nl>
2017-12-19 11:54:39 +0100

    Admin can be exempt

--- lib/account_controller_patch.rb
+++ lib/account_controller_patch.rb
@@ -1,46 +1,46 @@
 module GoogleAuthenticator
   module AccountControllerPatch
     extend ActiveSupport::Concern
-    
+
     included do
       before_action :delete_authenticating_cookie, only: :login
       before_action :restrict_if_no_user, only: [:two_step, :check_two_step]
-    
+
       def two_step
         #@user = User.authenticating
-        
+
         @user = User.active.find(session[:authenticating_user])
         @two_step_auth = TwoStepAuth.new
-      end    
-      
+      end
+
       def check_two_step
 
           @two_step_auth = TwoStepAuth.new
           user = TwoStepAuth.verify! session[:authenticating_user], params[:two_step_auth][:gauthcode].strip
           if user.is_a? User
-            
+
             session[:authenticating_user] = nil
             successful_authentication(user)
             update_sudo_timestamp! # activate Sudo Mode
           else
             redirect_to two_step_path, alert: "We couldn't validate you. Check the time"
           end
-       
+
       end
-      
-      
-      private    
-      
+
+
+      private
+
       def delete_authenticating_cookie
         session[:authenticating_user] = nil
       end
-      
+
       def restrict_if_no_user
         if session[:authenticating_user].blank?
           authorize and return
         end
       end
-      
+
       def authenticate_user
         if Setting.openid? && using_open_id?
           open_id_authenticate(params[:openid_url])
@@ -49,7 +49,7 @@ module GoogleAuthenticator
           two_step_auth
         end
       end
-      
+
       def two_step_auth
         user = User.try_to_login(params[:username], params[:password], false)
         if user.nil?
@@ -59,7 +59,7 @@ module GoogleAuthenticator
         else
           # Valid user
           if user.active?
-            if user.dual_auth? or TwoStepAuth.is_enforced?
+            if user.dual_auth? #or TwoStepAuth.is_enforced?
               session[:authenticating_user] = user.id
               redirect_to two_step_path
             else
@@ -72,8 +72,8 @@ module GoogleAuthenticator
           end
         end
       end
-      
+
     end
-    
+
   end # end of AccountControllerPatch
 end
