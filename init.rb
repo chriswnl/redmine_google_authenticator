cbc5b07a270463b227bb25de33da9aab568e4d55
Chris Willis <chris@twigtelecom.nl>
2017-12-19 12:16:12 +0100

    require_dependency and move modifiying modules into register block

--- init.rb
+++ init.rb
@@ -1,21 +1,18 @@
-require 'user_patch'
-require 'account_controller_patch'
+require_dependency 'user_patch'
+require_dependency 'account_controller_patch'
 require_dependency 'hooks'
 
-Rails.configuration.to_prepare do
-  User.send(:include, GoogleAuthenticator::UserPatch)
-  AccountController.send(:include, GoogleAuthenticator::AccountControllerPatch)
-  Setting.plugin_google_authenticator["two_step_auth"]
-end
 
 Redmine::Plugin.register :google_authenticator do
   name 'Google Authenticator'
   author 'Chris Willis'
   description 'Adds Google Authenitcator 2-step authentication'
   version '0.0.2'
-  
+
   settings default: {"two_step_auth" => {"by_user" => "disabled"} }, partial: 'settings/google_authenticator'
   #permission :view_ratings, :ratings => :index
   #menu(:top_menu, "Authentication", '/settings/authentication')
+  User.send(:include, GoogleAuthenticator::UserPatch)
+  AccountController.send(:include, GoogleAuthenticator::AccountControllerPatch)
+  # Setting.plugin_google_authenticator["two_step_auth"]
 end
-
